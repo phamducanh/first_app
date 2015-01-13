c135613e7ca713c4ab167922a53348856895a087
phamducanh <pham.duc.anh@framgia.com>
2015-01-13 09:01:56 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirtApp::Application.config.secret_key_base = 'd4e6999b1d11d76d4e9f5db461e8559f12b3fbec4bf2efaefddd6b181580538903597608ca016e9079c25d93cf595931b002fada823339e4747dfd11ca7e7bfd'
